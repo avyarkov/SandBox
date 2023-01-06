76f0deb4a3c06a91dccb93e0634b042cd6255ad0
avyarkov <jarkov.alexej@gmail.com>
2023-01-06 23:45:00 +0300

    Math.floorMod()

---
+++ src/main/java/_sandbox_/MathFloorMod.java
@@ -0,0 +1,20 @@
+package _sandbox_;
+
+public class MathFloorMod {
+    public static void main(String[] args) {
+        System.out.println("7 % 5 = " + 7 % 5);
+        System.out.println("-7 % 5 = " + -7 % 5);
+
+        System.out.println("Math.floorMod(7, 5) = " + Math.floorMod(7, 5));
+        System.out.println("Math.floorMod(-7, 5) = " + Math.floorMod(-7, 5));
+
+        System.out.println("-------");
+
+        System.out.println("7 / 5 = " + 7 / 5);
+        System.out.println("-7 / 5 = " + -7 / 5);
+
+        System.out.println("Math.floorDiv(7, 5) = " + Math.floorDiv(7, 5));
+        System.out.println("Math.floorDiv(-7, 5) = " + Math.floorDiv(-7, 5));
+
+    }
+}

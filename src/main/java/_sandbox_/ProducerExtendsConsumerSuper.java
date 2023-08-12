0809b3c26968e04c795e36b57baa10f23d4868ab
avyarkov <jarkov.alexej@gmail.com>
2023-08-13 02:25:36 +0300

    another generics oddity

--- src/main/java/_sandbox_/ProducerExtendsConsumerSuper.java
+++ src/main/java/_sandbox_/ProducerExtendsConsumerSuper.java
@@ -4,12 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@SuppressWarnings("unused")
 class ProducerExtendsConsumerSuper {
     private static class Class1 {}
     private static class Class2 extends Class1 {}
     private static class Class3 extends Class2 {}
 
-    @SuppressWarnings({"RedundantTypeArguments", "unused"})
+    @SuppressWarnings("RedundantTypeArguments")
     public static void main(String[] args) {
         List<? extends Class2> pe = List.<Class3>of(new Class3());
         // pe.add(new Class3());
@@ -28,5 +29,20 @@ class ProducerExtendsConsumerSuper {
         // Class2 class21 = optional.get();
         // java: incompatible types: capture#1 of ? super _sandbox_.ProducerExtendsConsumerSuper.Class2 cannot be converted to _sandbox_.ProducerExtendsConsumerSuper.Class2
         Object object1 = optional.get();
+
+        runAnotherOddity();
+    }
+
+    public static void runAnotherOddity() {
+        // double d = getMiddle(1, 2.3);
+        // java: incompatible types: inferred type does not conform to upper bound(s)
+        //    inferred: java.lang.Number&java.lang.Comparable<? extends java.lang.Number&java.lang.Comparable<?>&java.lang.constant.Constable&java.lang.constant.ConstantDesc>&java.lang.constant.Constable&java.lang.constant.ConstantDesc
+        //    upper bound(s): java.lang.Double,java.lang.Object
+        Number d = getMiddle(1, 2.3);
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <T> T getMiddle(T... a) {
+        return a[a.length / 2];
     }
 }
